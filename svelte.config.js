d7f27e4d98a3e2a1ccc3d2665a57b8baa2bcaab3
Andy Wang <andy_wang02@hotmail.com>
2024-12-01 19:29:51 -0500

    edited paths

--- svelte.config.js
+++ svelte.config.js
@@ -20,9 +20,9 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/game-dev-portfolio' : '',
-        }
+		// paths: {
+        //     base: process.env.NODE_ENV === 'production' ? '/game-dev-portfolio' : '',
+        // }
 	}
 };
 
